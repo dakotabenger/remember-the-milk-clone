e89055b16093de810ff5bb073362ebed2eba5724
Arjun <arjun.narain700@gmail.com>
2020-12-01 17:21:54 -0600

    initial tasks

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,9 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
+
+
+
+    document.getElementById('.listsubmit').addEventListener('click', async() => {
+        
+    })
 })
