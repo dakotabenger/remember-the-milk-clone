3c944b902a29f2aca2c4352fd8e0b70b5872f6dc
Dakota Benger <dakota@bengerlaw.com>
2020-12-01 11:14:13 -0700

    User Auth in progress

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,20 @@ const bcrypt = require('bcryptjs')
 
 
 /* GET users listing. */
-router.get('/sign-up')
+router.get('/sign-up',csrfProtection,asyncHandler(async (req,res) => {
+  const newUser = db.User.build();
+  res.render('sign-up', {
+    title: 'Register',
+    user,
+    csrfToken: req.csrfToken(),
+  });
+}))
 
-router.post('/sign-up', asyncHandler(async function(req, res, next) {
+router.post('/sign-up', csrfProtection, asyncHandler(async function(req, res, next) {
   const salt = await bcrypt.genSaltSync(10);
   const hashedPassword = await bcrypt.hash(req.body.password,salt)
   const newUser = await db.User.create({email: req.body.email,hashedPassword:hashedPassword,createdAt: Date.now(),updatedAt: Date.now()})
-  res.redirect("/tasks")
+  res.redirect("/list")
 }));
 
 module.exports = router;
