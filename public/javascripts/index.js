11de4c80c8da6845401ce04dd857b7e5642c1a33
Arianna Johnson <ariannajohnson588@gmail.com>
2020-12-01 20:40:04 -0500

    set up theme toggle functionality for sign-up page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,27 @@
-window.addEventListener("load", (event)=>{
+window.addEventListener("load", (event) => {
     console.log("hello from javascript!")
+})
+
+document.addEventListener('DOMContentLoaded', event => {
+
+    const toggleButton = document.getElementById('toggle-button');
+    const signUpImage = document.getElementById("image-container-id");
+    const signUpContainer = document.getElementById("sign-up-container-id");
+    const signUpButton = document.getElementById("sign-up-button-id");
+    const signUpHeader = document.getElementById("sign-up-header-id");
+    const signUpText = document.getElementById("sign-up-text-id");
+    const userInput = document.querySelectorAll("user-input");
+
+    toggleButton.addEventListener('click', () => {
+        signUpImage.classList.toggle('light');
+        toggleButton.classList.toggle('light');
+        signUpContainer.classList.toggle('light');
+        signUpButton.classList.toggle('light');
+        signUpHeader.classList.toggle('light');
+        signUpText.classList.toggle('light');
+
+        userInput.classList.toggle('light');
+
+    })
+
 })
