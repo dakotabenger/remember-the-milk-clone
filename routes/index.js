b4befacff5a6e335710a59586cbaec3b97997f8b
Dakota Benger <dakota@bengerlaw.com>
2020-12-02 16:27:44 -0700

    finished? create route

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const {asyncHandler,csrfProtection} = require("../ultils")
 const db = require('../db/models/index')
 const router = express.Router()
 
-router.get('/', requireAuth, asyncHandler(async (req, res) => {
+router.get('/', requireAuth,csrfProtection, asyncHandler(async (req, res) => {
   const user = req.session.auth.userId
   const tasks = await db.Task.findAll({ where: {user_id: user}, order:[['start_date', 'ASC']]}); //todo cant remember if this is how you get everything for a specific user
   res.render('index', {title: 'Home',tasks})
--- routes/tasks.js
+++ routes/tasks.js
@@ -9,8 +9,31 @@ const { check, validationResult } = require('express-validator');
 
 router.post("/",requireAuth,csrfProtection,taskValidators,asyncHandler(async (req,res) => {
         const validatorErrors = validationResult(req);
+        const userId = req.sessions.auth.userId
+        const {name} = req.body
         if (validatorErrors.isEmpty()) {
-                const newTask = await db.Task.create({})
+                const newTask = await db.Task.build({name:name,user_id:userId})
+                if (req.body.description){
+                        newTask.description = req.body.description
+                }
+                if (req.body.startDate) {
+                        newTask.start_date = req.body.startDate
+                }
+                if (req.body.endDate) {
+                        newTask.end_date = req.body.endDate
+                }
+                if (req.body.listId) {
+                        newTask.list_id = req.body.listId
+                }
+                if (req.body.tagId) {
+                        newTask.tag_id = req.body.tagId
+                }
+                if(req.body.priority) {
+                        newTask.priority = req.body.priority
+                }
+                await newTask.save()
+                res.json({newTask})
+
         } else {
                 const errors = validatorErrors.array().map((error) => error.msg);
                 res.json({errors})
