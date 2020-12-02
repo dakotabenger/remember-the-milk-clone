98a6cd3e4ef646ada0d29989d7143daa2d8a6ced
Arianna Johnson <ariannajohnson588@gmail.com>
2020-12-02 13:03:40 -0500

    light and dark css completed for login page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,23 +6,21 @@ document.addEventListener('DOMContentLoaded', event => {
 
     const toggleButton = document.getElementById('toggle-button');
     const image = document.querySelectorAll(".image-container");
-    const signUpContainer = document.getElementById("sign-up-container-id");
+    const signUpContainer = document.querySelectorAll(".sign-up-container");
+    const signUpHeader = document.querySelectorAll(".sign-up-header");
     const signUpButton = document.getElementById("sign-up-button-id");
-    const signUpHeader = document.getElementById("sign-up-header-id");
-    const signUpText = document.getElementById("sign-up-text-id");
+    const signUpText = document.querySelectorAll(".sign-up-text");
     const userInput = document.querySelectorAll(".user-input");
 
-    console.log(userInput);
 
     toggleButton.addEventListener('click', () => {
         toggleButton.classList.toggle('light');
+        signUpHeader.forEach(ele => ele.classList.toggle('light'));
         image.forEach(ele => ele.classList.toggle('light'));
         userInput.forEach(ele => ele.classList.toggle('light'));
-        signUpContainer.classList.toggle('light');
+        signUpContainer.forEach(ele => ele.classList.toggle('light'));
+        signUpText.forEach(ele => ele.classList.toggle('light'));
         signUpButton.classList.toggle('light');
-        signUpHeader.classList.toggle('light');
-        signUpText.classList.toggle('light');
-
 
     });
 });
