e5e2c4bdbb4b64a3053135eb393931185f554c2c
Dakota Benger <dakota@bengerlaw.com>
2020-12-03 09:54:15 -0700

    fixed list model

--- db/models/list.js
+++ db/models/list.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const List = sequelize.define('List', {
-    description: DataTypes.STRING,
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    user_id: DataTypes.INTEGER
   }, {});
   List.associate = function(models) {
 List.hasMany(models.Task, {foreignKey: "list_id"})
