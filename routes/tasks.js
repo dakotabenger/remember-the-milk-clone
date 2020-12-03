3b88e7941f26b00771175f6ba491779de65246eb
Arianna Johnson <ariannajohnson588@gmail.com>
2020-12-02 19:08:33 -0500

    created route for deleting a single task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,26 +1,26 @@
-const {requireAuth} = require("../auth")
+const { requireAuth } = require("../auth")
 const express = require('express')
-const {asyncHandler,csrfProtection} = require("../ultils")
+const { asyncHandler, csrfProtection } = require("../ultils")
 const db = require('../db/models/index')
 const router = express.Router()
 const { check, validationResult } = require('express-validator');
 
 const taskValidators = [
         check('name')
-          .exists({ checkFalsy: true })
-          .withMessage('Please provide a task'),
+                .exists({ checkFalsy: true })
+                .withMessage('Please provide a task'),
         check('description')
-          .isLength({max:100})
-          .withMessage("Description must be less than 100 characters")
-        ];
+                .isLength({ max: 100 })
+                .withMessage("Description must be less than 100 characters")
+];
 
-router.post("/",requireAuth,csrfProtection,taskValidators,asyncHandler(async (req,res) => {
+router.post("/", requireAuth, csrfProtection, taskValidators, asyncHandler(async (req, res) => {
         const validatorErrors = validationResult(req);
         const userId = req.sessions.auth.userId
-        const {name} = req.body
+        const { name } = req.body
         if (validatorErrors.isEmpty()) {
-                const newTask = await db.Task.build({name:name,user_id:userId})
-                if (req.body.description){
+                const newTask = await db.Task.build({ name: name, user_id: userId })
+                if (req.body.description) {
                         newTask.description = req.body.description
                 }
                 if (req.body.startDate) {
@@ -35,23 +35,45 @@ router.post("/",requireAuth,csrfProtection,taskValidators,asyncHandler(async (re
                 if (req.body.tagId) {
                         newTask.tag_id = req.body.tagId
                 }
-                if(req.body.priority) {
+                if (req.body.priority) {
                         newTask.priority = req.body.priority
                 }
                 await newTask.save()
-                res.json({newTask})
+                res.json({ newTask })
 
         } else {
                 const errors = validatorErrors.array().map((error) => error.msg);
-                res.json({errors})
-        }       
+                res.json({ errors })
+        }
 }))
 
 // Delete single task route
-router.delete("/:id")
+router.delete(
+        "/:id",
+        asyncHandler(async (req, res, next) => {
+                const task = await Task.findOne({
+                        where: {
+                                id: req.params.id,
+                        },
+                });
+                if (task) {
+                        if (req.session.auth.userId !== task.user_id) {
+                                const err = new Error('Unauthorized');
+                                err.status = 401;
+                                err.message = 'You are not authorized to delete this task.';
+                                err.title = 'Unauthorized';
+                                throw err;
+                        }
+                        await task.destroy();
+                        res.json({ message: "Deleted task." });
+                } else {
+                        next(taskNotFoundError(req.params.id));
+                }
+        })
+);
 
 
-// Edit Single task route 
+// Edit Single task route
 router.patch("/:id")
 
 // Add Task to List
